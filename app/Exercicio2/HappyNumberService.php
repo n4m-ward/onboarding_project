79850ef032706a95272e76ae7bbc5c9e69c0255a
n4m-ward <gabrielphp00@gmail.com>
2022-05-17 17:22:11 -0300

    - finalizado testes de todos os metodos criados via tdd. - feito metodos do exercicio 2

--- app/Exercicio2/HappyNumberService.php
+++ app/Exercicio2/HappyNumberService.php
@@ -4,19 +4,35 @@ namespace Onboarding\Exercicio2;
 
 class HappyNumberService
 {
-    public function isAnHappyNumber(int $number, array $previousNumber = []): bool
+    public function isAnHappyNumber(int $number, array $previousNumbers = []): bool
     {
-        if ($number == 0) {
+        if ($number == 1) {
             return true;
         }
 
-        if (in_array($number, $previousNumber)) {
+        if (in_array($number, $previousNumbers)) {
             return false;
         }
+        $previousNumbers[] = $number;
+        $numberAfterSquareAndSum = $this->squareAndSumEachDigitOfNumber($number);
+
+        return $this->isAnHappyNumber($numberAfterSquareAndSum, $previousNumbers);
+    }
+
+    private function squareAndSumEachDigitOfNumber(int $number): int
+    {
+        $numberToReturn = 0;
+        $splittedNumberArray = str_split($number);
+
+        foreach ($splittedNumberArray as $number) {
+            $numberToReturn += $this->squareNumber($number);
+        }
+
+        return $numberToReturn;
     }
 
     private function squareNumber(int $number): int
     {
-
+        return $number * $number;
     }
 }
--- tests/Exercicio2/HappyNumberServiceTest.php
+++ tests/Exercicio2/HappyNumberServiceTest.php
@@ -21,4 +21,74 @@ class HappyNumberServiceTest extends TestCase
 
         $this->assertTrue($isAnHappyNumber);
     }
+
+    public function testIsAnHappyNumberServiceReturnFalse(): void
+    {
+        $numberToVerify = 2;
+        $isAnHappyNumber = $this->happyNumberService->isAnHappyNumber($numberToVerify);
+
+        $this->assertFalse($isAnHappyNumber);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testsquareAndSumEachDigitOfNumberBringsCorrectResultForTwoDigitsNumber(): void
+    {
+        $expectedResult = 8;
+        $numberToSquare = 22;
+        $result = $this->invokeNonPublicProperty(
+            $this->happyNumberService,
+            'squareAndSumEachDigitOfNumber',
+            [$numberToSquare]
+        );
+
+        $this->assertEquals($result, $expectedResult);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testsquareAndSumEachDigitOfNumberBringsCorrectResultForOneDigitNumber(): void
+    {
+        $expectedResult = 4;
+        $numberToSquare = 2;
+        $result = $this->invokeNonPublicProperty(
+            $this->happyNumberService,
+            'squareAndSumEachDigitOfNumber',
+            [$numberToSquare]
+        );
+
+        $this->assertEquals($result, $expectedResult);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testsquareNumberWorks(): void
+    {
+        $expectedResult = 4;
+        $numberToSquare = 2;
+        $result = $this->invokeNonPublicProperty(
+            $this->happyNumberService,
+            'squareNumber',
+            [$numberToSquare]
+        );
+
+        $this->assertEquals($result, $expectedResult);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    function invokeNonPublicProperty($object, $methodName, array $arguments = [])
+    {
+        $class = new ReflectionClass($object);
+        $method = $class->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return empty($arguments)
+            ? $method->invoke($object)
+            : $method->invokeArgs($object, $arguments);
+    }
 }
